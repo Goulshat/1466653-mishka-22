751a84202dd1275260c509ab1af6278173453aef
Goulshat <76894691+Goulshat@users.noreply.github.com>
2021-05-20 18:53:53 +0500

    Добавляет состояние ошибки пустым полям

--- source/js/script.js
+++ source/js/script.js
@@ -23,25 +23,45 @@ const modal = document.querySelector(".modal-order");
 const addCartButtons = document.querySelectorAll(".js-add-to-cart");
 const modalForm = document.querySelector(".modal-order__form");
 
-// при нажатии на ссылку "заказать" открывается попап
-for (let addCartButton of addCartButtons) {
-  addCartButton.addEventListener("click", function(evt) {
+
+if (modal) {
+  // при нажатии на ссылку "заказать" открывается попап
+  for (let addCartButton of addCartButtons) {
+    addCartButton.addEventListener("click", function(evt) {
+      evt.preventDefault();
+      modal.classList.add("modal-order--open");
+    });
+  }
+  // отправка формы: закрывается попап
+  modalForm.onsubmit = function (evt) {
     evt.preventDefault();
-    modal.classList.add("modal-order--open");
+    modal.classList.remove("modal-order--open");
+  }
+  // ESC закрывает попап
+  window.addEventListener("keydown", function(evt) {
+    if (evt.keyCode === 27) {
+      if (modal.classList.contains("modal-order--open")) {
+        modal.classList.remove("modal-order--open");
+      }
+    }
   });
 }
 
-// отправка формы: закрывается попап
-modalForm.onsubmit = function (evt) {
-  evt.preventDefault();
-  modal.classList.remove("modal-order--open");
-}
 
-// ESC закрывает попап
-window.addEventListener("keydown", function(evt) {
-  if (evt.keyCode === 27) {
-    if (modal.classList.contains("modal-order--open")) {
-      modal.classList.remove("modal-order--open");
+/* Форма заказа */
+const form = document.querySelector(".order-form");
+const submitButton = document.querySelector(".order-form__button");
+const inputsRequired = document.querySelectorAll(".js-input-required");
+
+if (form) {
+  submitButton.addEventListener("click", function(evt) {
+    for (let inputRequired of inputsRequired) {
+      if (!inputRequired.value) {
+        evt.preventDefault();
+        inputRequired.classList.add("form-label__input--error");
+      } else {
+        inputRequired.classList.remove("form-label__input--error");
+      }
     }
-  }
-});
+  });
+}
